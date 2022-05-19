c0ed67ec3c482d270b1c2c47df953cebdd5e0b94
Gedeson Wasley <gedeson.wasley@gmail.com>
2022-05-19 14:43:08 +0100

    Create gedPackage.php

---
+++ gedPackage.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ * Created by : Gedeson
+ * ProjectName : GedPackage
+ * Purpose : Submit to packagist
+ */
+
+namespace gedPackage;
+
+class gedPackage {
+    public function pack() {
+        return "Hello Packagist! I am coming";
+    }
+}
